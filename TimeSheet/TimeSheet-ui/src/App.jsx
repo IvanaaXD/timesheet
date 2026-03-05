bbf2d9b0c7598615b9ff739e36e1d4b507cadd40
Ivana Radovanovic <ivana.radovanovic@vega.local>
2026-03-05 16:11:55 +0100

    add react project

---
+++ TimeSheet/TimeSheet-ui/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'Arial' }}>
+      <h1>TimeSheet Project</h1>
+      <p></p>
+      <hr />
+      <p></p>
+    </div>
+  )
+}
+
+export default App
